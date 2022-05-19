37057b3cce19f2093967d4c2995c88644b5fc4d5
theonnogueira <101525395+theonnogueira@users.noreply.github.com>
2022-05-18 22:20:27 -0300

    Add files via upload

---
+++ backend/src/main/java/repositories/PostagemRepository.java
@@ -0,0 +1,15 @@
+package repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import models.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+	@Query(nativeQuery = true, value="SELECT * FROM tb_postagens WHERE tb_postagens.titulo LIKE %:titulo%")
+	List<Postagem> findPostagensByTitulo(String titulo);
+		
+}
---
+++ backend/src/main/java/repositories/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import models.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	public Optional<Usuario> findByUsuario(String usuario);
+
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+
+}
